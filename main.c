7629479bb06171faf3f0c1192b6c5c88e12c8efd
Elisey <elisey.rav@gmail.com>
2017-06-12 00:14:15 +0300

    Init commit with Unity

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("hello rer\r\n");
+	return 0;
+}
---
+++ test/Test_Storage.c
@@ -0,0 +1,14 @@
+#include "unity.h"
+
+void setUp()
+{
+}
+
+void tearDown()
+{
+}
+
+void test_firtsTest()
+{
+	TEST_ASSERT(0);
+}
